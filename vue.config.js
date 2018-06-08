ccca2db1ebd5d6e654da9266df4dfe579b6a032d
Caideniao <yzqtdu@gmail.com>
2018-06-08 18:22:59 +0800

    modify assets route

--- vue.config.js
+++ vue.config.js
@@ -6,10 +6,10 @@ module.exports = {
     // sub-path here. For example, if your app is deployed at
     // https://www.foobar.com/my-app/
     // then change this to '/my-app/'
-    baseUrl: '/resume-editor/',
+    baseUrl: './',
   
     // where to output built files
-    outputDir: './dist',
+    outputDir: 'dist',
   
     // where to put static assets (js/css/img/font/...)
     assetsDir: 'static',
